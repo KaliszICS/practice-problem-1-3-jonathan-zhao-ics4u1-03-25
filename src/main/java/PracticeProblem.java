724a1a6d085a8df6a4d678c1973e1a61da35fe72
jonathan-zhao-ics4u1-03-25 <jonathan.zhao@student.tdsb.on.ca>
2025-03-05 21:48:48 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
+	public static String readFile(String filename) {
+		BufferedReader reader = null;
+		String file = "";
+		try {
+			reader = new BufferedReader(new FileReader(filename));
+			while (reader.ready()) {
+				file += (char) reader.read();
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+			
+		} catch (IOException e) {
+			System.out.println(e);
 
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+		}
+		return file.strip();
+	}
+
+	public static String backwardsReadFile(String fileName) {
+		BufferedReader reader = null;
+		String file = "";
+		try {
+			reader = new BufferedReader(new FileReader(fileName));
+			while (reader.ready()) {
+				file = (char) reader.read() + file;
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+
+		} catch (IOException e) {
+			System.out.println(e);
+
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+		}
+		return file.strip();
+	}
 }
